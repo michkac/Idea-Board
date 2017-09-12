6d11cc568d8b62be493e4131e515e30b8d0ffc6d
Michelle Chung <michka.chung@gmail.com>
2017-09-12 00:03:28 -0400

    added functionality to app

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,72 +1,160 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import logo from '../styles/assets/logo.png';
-import firebase from './firebase.js'
+import firebase, {provider, auth} from './firebase.js';
 
-// const dbRef = firebase.database().ref("/items");
+const cardRef = firebase.database().ref('/cards');
 
+// landing page
+class Landing extends Component {
+	render(){
+		return(
+			<div className="login-page">
+				<div className="headerContent">
+					<h1>Idea Garden</h1>
+					<p>Plant your ideas here.</p>
+					<button className="login-btn" onClick={this.props.login}>Log In</button>
+				</div>
+				<img className="logo" src="../../dev/styles/assets/logo2.png"/>
+			</div>
+		)
+	}
+}
 
-// change user.loggedIn: true when user logs in and changes the state of the app
-// class Login extends React.Component {
-// 	constructor() {
-// 		super();
-// 		this.state = {
-// 			user = {
-// 				loggedIn: false
-// 			}
-// 		}
-// 	}
-// }
-
-
-//have a header that floats logo, name, new idea and logout in one line
+// logged in home header
 class Header extends Component {
 	render(){
 		return(
 			<div className="logged-in-home">
-				<img src="../styles/assets/logo.png"/>
-				<p>name of user</p>
-				<h1>Idea Garden</h1>
-				<button className="log-out-btn">Log Out</button>
-				<button className="new-idea-btn">New Idea</button> 
+				<div className="header-left">
+					<img className="logo" src="../../dev/styles/assets/logo2.png"/>
+					<p>user name's</p>
+					<h1>Idea Garden</h1>
+				</div>
+				<div className="header-right">
+					<button className="new-idea-btn" onClick={() => this.props.createForm()} >New Idea</button>
+					<button className="log-out-btn" onClick={this.props.logout}>Log out</button>
+				</div>
 			</div>
 		)
 	}
 }
 
-
-// display ideas in the form of square tiles
-// function for editing existing ideas and delete ideas
+// container for displaying ideas
 class CardContainer extends Component {
 	render() {
 		return (
-			<div className= "card-content">
-			</div>
+			<ul className= "card-content">
+				{this.props.cards.map( (card, index)=> {
+					return(
+						<li key={index}>
+							<h3>{card.title}</h3>
+							<p>{card.details}</p>
+						</li>
+					)
+				})}
+			</ul>
 		)
 	}
 }
-
 
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
+			ideaName: '',
+			ideaDetails: '',
+			showForm: false,
+			showCard: false,
+			cards: [],
+			user: null,
 		}
-
-		// this.handleCardClicked = this.handleCardClicked.bind(this);
+		this.createForm = this.createForm.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
+	}
+	login(e) {
+		e.preventDefault()
+		auth.signInWithPopup(provider) 
+		.then((result) => {
+			const user = result.user;
+			this.setState({ user });
+		});
+	}
+	logout(e) {
+		e.preventDefault();
+		auth.signOut()
+		.then(() => {
+			this.setState({ user: null });
+		});
+	}
+	handleChange(e) {
+	    this.setState({
+	    	[e.target.name]: e.target.value
+	    });
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		const card = {
+			ideatitle: this.state.ideaName,
+			ideadetails: this.state.ideaDetails
+		}
+		cardRef.push(card);
+		this.setState({
+			ideatitle: '',
+			ideadetails: ''
+		});
+	}
+	componentDidMount() {
+		cardRef.on('value', (snapshot) => {
+			let cards = snapshot.val();
+			let newState = [];
+			for (let card in cards) {
+		    	newState.push({
+		    		id: card,
+		    		title: cards[card].ideatitle,
+		    		details: cards[card].ideadetails,
+		    	});
+		    }
+		    this.setState({
+		    	cards: newState
+		    });
+		});
+	}
+	removeItem(itemId) {
+		const cardRef = firebase.database().ref(`/cards/${cardId}`);
+		cardRef.remove();
+	}
+	createForm(){
+		this.setState({
+			showForm: true
+		})
 	}
     render() {
-      return (
-        <main>
-	        <Header />
-	        <CardContainer />
-        </main>
-      )
+	    let showForm = (
+	    	<form className="new-idea-form" onSubmit={this.handleSubmit}>
+	    		<button className="close-btn">
+	    			<img src="../../dev/styles/assets/close.png" />
+	    		</button>
+	    		<input className="ideaName" type="text" name= "ideaName" placeholder="Idea title" onChange={this.handleChange} value={this.state.ideaName}/>
+	    		<textarea className="ideaDetails" name="ideaDetails" cols="30" rows="8" placeholder="Idea details" onChange={this.handleChange} value={this.state.ideaDetails}/>
+	    		<button className="create-btn">Create</button>
+	    	</form>
+	    )
+	    return (
+	        <main>
+		        <div className="wrapper">
+		        	{ !this.state.user && <Landing login={this.login} />}
+			        { this.state.user && <Header createForm={this.createForm} logout={this.logout} />}
+			        <div className="add-card">
+			        	{this.state.showForm === true ? showForm : null}
+			        </div>
+			        { this.state.user && <CardContainer cards={this.state.cards}/>}
+		        </div>
+	        </main>
+	    )
     }
-    // componentDidMount() {
-
-    // }
 }
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- dev/scripts/firebase.js
+++ dev/scripts/firebase.js
@@ -9,4 +9,6 @@ import firebase from 'firebase'
     messagingSenderId: "759290521804"
   };
   firebase.initializeApp(config);
+  export const provider = new firebase.auth.GoogleAuthProvider();
+  export const auth = firebase.auth();
   export default firebase;
